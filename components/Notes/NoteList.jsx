f9d9b5e03aa28cd375d44610e3695d1defb30b03
satyamgit1 <satyamsingh2003a@gmail.com>
2025-09-06 15:03:01 +0530

    added project issue same project script type is same

--- components/Notes/NoteList.jsx
+++ components/Notes/NoteList.jsx
@@ -2073,7 +2073,7 @@ export default function NoteList({ userId }) {
                 d="M3 4a1 1 0 011-1h16a1 1 0 011 1v2.586a1 1 0 01-.293.707l-6.414 6.414a1 1 0 00-.293.707V17l-4 4v-6.586a1 1 0 00-.293-.707L3.293 7.293A1 1 0 013 6.586V4z"
               />
             </svg>
-            Refine Your Collection
+            Search your script collection
           </h2>
           <div className="flex items-center space-x-4">
             <span className="text-sm font-medium text-white bg-gradient-to-r from-emerald-500 to-green-500 px-4 py-2 rounded-full shadow-md">
